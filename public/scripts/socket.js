c773c07fd60fa9551828de55203589bdb859de51
BryanYuen <bryanyuen5@gmail.com>
2025-04-27 21:35:53 +0800

    Setup Basic Server

---
+++ public/scripts/socket.js
@@ -0,0 +1,38 @@
+const Socket = (function() {
+    // This stores the current Socket.IO socket
+    let socket = null;
+
+    // This function gets the socket from the module
+    const getSocket = function() {
+        return socket;
+    };
+
+    // This function connects the server and initializes the socket
+    const connect = function() {
+        socket = io();
+
+        // Wait for the socket to connect successfully
+        socket.on("connect", () => {          
+            console.log("browser successfully connected");
+        });
+
+        socket.on("greeting", () => {
+            console.log("Browser side ok!");
+        })
+    };
+
+    // This function disconnects the socket from the server
+    const disconnect = function() {
+        socket.disconnect();
+        socket = null;
+    };
+
+    // This function sends a post message event to the server
+    const postMessage = function(content) {
+        if (socket && socket.connected) {
+            socket.emit("post message", content);
+        }
+    };
+
+    return { getSocket, connect, disconnect, postMessage };
+})();
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const session = require("express-session");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const onlineUsers = {};
+
+// Create the Express app
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+io.on("connection", (socket) => {
+    socket.emit("greeting");
+})
+
+// Use a web server to listen at port 8000
+httpServer.listen(8000, () => {
+    console.log("The chat server has started...");
+});
